f385c107979ccf5a932160d17f1669144b4acffa
Taiki-ss <taiki.ishida.ss@gmail.com>
2023-05-24 15:41:51 +0900

    余計なやつが残っていた

--- .github/script.sh
+++ .github/script.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-LAST_COMMENT_URL=$(curl -s -H "Authorization: token $GITHUB_TOKEN" $COMMENTS_URL | jq -r '.[-1].url')
-
 MERGE_COMMIT_MESSAGE=$(curl -s -H "Authorization: token ${{ GITHUB_TOKEN }}" https://api.github.com/repos/${{ REPOSITORY }}/commits/${{ MERGE_COMMIT_SHA }} | jq -r .commit.message)
 
 echo "$MERGE_COMMIT_MESSAGE"
