2a8da829644face63ee802800ee11570361f071a
Taiki-ss <taiki.ishida.ss@gmail.com>
2023-05-24 15:48:59 +0900

    変数修正

--- .github/script.sh
+++ .github/script.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-MERGE_COMMIT_MESSAGE=$(curl -s -H "Authorization: token ${{ GITHUB_TOKEN }}" https://api.github.com/repos/${{ REPOSITORY }}/commits/${{ MERGE_COMMIT_SHA }} | jq -r .commit.message)
+MERGE_COMMIT_MESSAGE=$(curl -s -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/repos/$REPOSITORY/commits/$MERGE_COMMIT_SHA | jq -r .commit.message)
 
 echo "$MERGE_COMMIT_MESSAGE"
